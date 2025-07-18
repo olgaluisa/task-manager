9f316f390cc46957d2fef3d4f465c95c690dfcb2
olgaluisa <luiisaolga11@gmail.com>
2025-07-17 21:28:49 -0300

    feat: add svg icons to Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,6 @@
 import SidebarButton from './SidebarButton'
+import HomeIcon from '../assets/icons/home.svg?react'
+import TasksIcon from '../assets/icons/tasks.svg?react'
 
 const Sidebar = () => {
   return (
@@ -11,8 +13,14 @@ const Sidebar = () => {
         </p>
       </div>
       <div className="flex flex-col gap-2 p-2">
-        <SidebarButton variant="unselected">Inicio</SidebarButton>
-        <SidebarButton variant="selected">Minhas tarefas</SidebarButton>
+        <SidebarButton variant="unselected">
+          <HomeIcon />
+          Inicio
+        </SidebarButton>
+        <SidebarButton variant="selected">
+          <TasksIcon />
+          Minhas tarefas
+        </SidebarButton>
       </div>
     </section>
   )
