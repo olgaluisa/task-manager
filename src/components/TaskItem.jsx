2cb9f4562f36fba4dba6c67f96a65ae122ca1116
olgaluisa <luiisaolga11@gmail.com>
2025-07-28 22:45:43 -0300

    feat: ensure TaskItem component changes depending on task status

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,20 +1,53 @@
+import { Check, Loader, ExternalLink, Trash2 } from 'react-feather'
+import Button from './Button'
 const TaskItem = ({ task }) => {
   const getStatusClasses = () => {
     if (task.status === 'done') {
-      return 'bg-[#00adb5]/10 text-[#00adb5]'
+      return 'bg-[#00adb5] text-[#00adb5]'
     }
     if (task.status === 'in_progress') {
-      return 'bg-[#ffaa84]/10  text-[#ffaa84]'
+      return 'bg-[#ffaa84]  text-[#ffaa84]'
     }
     if (task.status === 'not_started') {
       return 'bg-[#35383e]/10  text-[#35383e]'
     }
   }
+  const getStatusColor = () => {
+    if (task.status === 'done') return '#00adb5'
+    if (task.status === 'in_progress') return '#ffaa84'
+    if (task.status === 'not_started') return '#35383e'
+  }
   return (
     <div
-      className={`flex items-center rounded-lg px-4 py-3 text-sm ${getStatusClasses()}`}
+      className={`flex items-center justify-between gap-2 rounded-lg px-4 py-3 text-sm bg-[${getStatusColor()}]/10 ${getStatusClasses()}`}
     >
-      {task.title}
+      <div className="flex items-center gap-2">
+        <label
+          className={`relative flex h-7 w-7 cursor-pointer items-center justify-center rounded-lg ${getStatusClasses()} `}
+        >
+          <input
+            type="checkbox"
+            checked={task.status === 'done'}
+            className="absolute h-full w-full cursor-pointer opacity-0"
+          />
+          {task.status === 'done' && <Check className="text-white" />}
+          {task.status === 'in_progress' && (
+            <Loader className="animate-spin text-white" />
+          )}
+        </label>
+        {task.title}
+      </div>
+      <div className="flex items-center gap-2">
+        <Button variant="ghost">
+          <Trash2 size={20} className="cursor-pointer" />
+        </Button>
+        <a href="/">
+          <ExternalLink
+            size={20}
+            className="cursor-pointer text-[#818181] transition hover:opacity-75"
+          />
+        </a>
+      </div>
     </div>
   )
 }
